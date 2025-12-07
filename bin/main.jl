a666bf78a6cd708d3e90344cf0e23a7142043ec4
Takuya Iwanaga <takuyai@gmail.com>
2025-12-07 16:17:58 +1100

    Further adjustments

--- bin/main.jl
+++ bin/main.jl
@@ -1,12 +1,30 @@
 include(joinpath(@__DIR__, "src", "main.jl"))
 
-# Use get_server() for automatic JuliaHub proxy detection
-server = Bonito.get_server()
-route!(server, "/" => app)
+proxy = get(ENV, "JULIAHUB_APP_URL", "")
+if isempty(proxy)
+    @info "No Bonito proxy found in environment variable JULIAHUB_APP_URL"
+else
+    @info "Using Bonito proxy from JULIAHUB_APP_URL: $proxy"
+end
+
+port = get(ENV, "PORT", "8080") # it's guaranteed this exists on JuliaHub
+@info "Constructing Bonito server on 0.0.0.0:$port $(isempty(proxy) ? "" : "with proxy $proxy")"
+server = Bonito.Server("0.0.0.0", parse(Int, port); proxy_url=proxy, verbose=-1)
 
 # Display URL
 url_to_visit = online_url(server, "/")
-@info url_to_visit
+@info "Website launched at: $(url_to_visit)"
 
-# Keep server running
+# path = normpath(joinpath(dirname(pathof(BonitoBook)), "..", "docs", "examples", "intro.md"))
+
+# app = Bonito.App() do
+#     return BonitoBook.Book(path)
+# end;
+
+route!(server, "/" => app)
+@info server
+
+# Wait for the server to exit, because if running in an app, the app will
+# exit when the script is done.  This makes sure that the app is only closed
+# if (a) the server closes, or (b) the app itself times out and is killed externally.
 wait(server)
