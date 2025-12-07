c53154568cda31b1b25b5579476e5f61708897c8
Takuya Iwanaga <takuyai@gmail.com>
2025-12-07 15:02:50 +1100

    Adjust include path
    Adjustment for JuliaHub, let's see if this works...

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,6 @@
-include("../src/main.jl")
+include(joinpath(@__DIR__, "src", "main.jl"))
 
+# Use get_server() for automatic JuliaHub proxy detection
 server = Bonito.get_server()
 route!(server, "/" => app)
 
